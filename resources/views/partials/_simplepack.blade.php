a7fe02b8be120208308bc39506f7906d180264a4
Gauthamram <gthm.ram@gmail.com>
2017-09-03 23:45:19 +0200

    Revert "change of barcode type"
    This reverts commit 8925d0646e115fd98908ee94e3540a5d35ff6547.

--- resources/views/partials/_simplepack.blade.php
+++ resources/views/partials/_simplepack.blade.php
@@ -12,7 +12,7 @@
 			</div>
 			<div class="third-row">
 				<div class="barcode">
-					<img src="data:image/png;base64,{{DNS1D::getBarcodePNG($simplepack['barcode'], 'EAN13',1,60)}}" alt="barcode" />
+					<img src="data:image/png;base64,{{DNS1D::getBarcodePNG($simplepack['barcode'], 'EAN8',1,60)}}" alt="barcode" />
 					{{$simplepack['barcode']}}
 				</div>
 			</div>
