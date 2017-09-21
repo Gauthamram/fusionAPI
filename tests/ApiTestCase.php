b65f72e0eeab73289f8e727b856c24f7ab10c1d6
Gauthamram <gthm.ram@gmail.com>
2017-09-21 21:34:53 +0200

    Initial Testing

---
+++ tests/ApiTestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+
+class ApiTestCase extends TestCase
+{
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,9 @@
 <?php
+namespace Tests;
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
 {
     /**
      * The base URL to use while testing the application.
@@ -8,6 +11,19 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      * @var string
      */
     protected $baseUrl = 'http://localhost';
+
+    public function setToken()
+    {
+        $response = $this->Json('POST', 'api/auth/login', ['email' => 'gthm.ram@gmail.com', 'password' => '123456789']);
+        
+        $content = json_decode($response->getContent());
+        
+        $this->assertObjectHasAttribute('token', $content, 'Token does not exists');
+        
+        $this->token = $content->token;
+        
+        return $this;
+    }
 
     /**
      * Creates the application.
@@ -18,7 +34,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
